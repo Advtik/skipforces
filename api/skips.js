fa0a67f17f28be929c630315de144483d481995c
Adwiteek <150584084+Advtik@users.noreply.github.com>
2025-09-14 21:08:50 +0530

    Update skips.js

--- api/skips.js
+++ api/skips.js
@@ -17,6 +17,7 @@ export default async function handler(req, res) {
         if (result.result[i].verdict == "SKIPPED") {
           if (!arr.includes(result.result[i].contestId)) {
             arr.push(result.result[i].contestId);
+            
           }
         }
       }
