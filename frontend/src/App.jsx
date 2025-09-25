5cc24f5a62d72f43e40a71ae7a144aea55ba232e
Adwiteek <sadwiteek@gmail.com>
2025-09-25 16:47:20 +0530

    added particle js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import {TakeInput} from './components/Input'
+import ParticlesComponent from './components/Particles'
 
 function App() {
 
   return (
     <div>
+      <ParticlesComponent id="particles"></ParticlesComponent>
       <TakeInput></TakeInput>
     </div>
   );
